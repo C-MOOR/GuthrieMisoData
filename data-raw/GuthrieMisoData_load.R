d963f7b4ce615f3632c9eb051f4170e7d54609e3
javibio-git <jcarpinteyro@carnegiescience.edu>
2025-05-16 16:28:09 -0400

    First object

--- data-raw/GuthrieMisoData_load.R
+++ data-raw/GuthrieMisoData_load.R
@@ -1,6 +1,15 @@
 ## GuthrieMisoData_load.R
 
+# load libraries
+library(tidyverse)
 
-miso_clean <- readRDS("MISO_clean.rds")
 
-miso_prop <- readRDS("MISO_16S.prop.clean.rds")
+# load data
+testdriveR_raw <- read_tsv("data-raw/test-driveR.tsv", col_names = TRUE)
+
+# Tidying data
+testdriveRData <- type_convert(testdriveR_raw)
+
+
+# This needs to be run last within the script
+usethis::use_data(testdriveRData, overwrite = TRUE)
